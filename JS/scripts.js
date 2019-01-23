a8f93e5a5697801f34e44ff6da6f88269a157a0b
Lawrence <lhebia@gmail.com>
2019-01-23 13:20:54 -0500

    Attempt menu in function

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,5 +2,20 @@ console.log("Hello!");
 
 function myClickFunction(x) {
     x.classList.toggle("change");
+
+    // How can I get this same function to also make the menu change display properties
+
+    var mobileMenuDisplay = document.getElementById("theMobileMenu").style.display;
+    
+    if (mobileMenuDisplay == "none") {
+        // set display proerty to "block"
+        mobileMenuDisplay.style.display = "block";
+    }
+        else {
+        // set diplay property to "none"
+        mobileMenuDisplay.style.dispaly = "none";
+    }
+
     console.log("worked");
 }
+
